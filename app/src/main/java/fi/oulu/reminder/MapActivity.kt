1598be915e3005255976627fed79abd1d8a5f410
kennedyopokuasare <opokuasarekennedy@gmail.com>
2020-02-07 14:40:26 +0200

    added Google Maps

--- app/src/main/java/fi/oulu/reminder/MapActivity.kt
+++ app/src/main/java/fi/oulu/reminder/MapActivity.kt
@@ -1,18 +1,41 @@
 package fi.oulu.reminder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.room.Room
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_map.*
 import org.jetbrains.anko.doAsync
 
-class MapActivity : AppCompatActivity() {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    lateinit var gMap: GoogleMap
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    val LOCATION_REQUEST_CODE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        // TODO: map stuff
+        (map_fragment as SupportMapFragment).getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         map_create.setOnClickListener {
 
             // dummy
@@ -24,8 +47,9 @@ class MapActivity : AppCompatActivity() {
             )
 
             doAsync {
-                val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java,"reminders")
-                    .build()
+                val db =
+                    Room.databaseBuilder(applicationContext, AppDatabase::class.java, "reminders")
+                        .build()
 
                 db.reminderDao().insert(reminder)
                 db.close()
@@ -34,4 +58,76 @@ class MapActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    override fun onMapReady(map: GoogleMap?) {
+        gMap = map ?: return
+
+        if (!isLocationPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                LOCATION_REQUEST_CODE
+            )
+        } else {
+            gMap.isMyLocationEnabled = true
+
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+
+                if (location != null) {
+                    with(gMap) {
+                        val latLong = LatLng(location.latitude, location.longitude)
+                        animateCamera(CameraUpdateFactory.newLatLngZoom(latLong, 13f))
+                        //addMarker(MarkerOptions().position(latLong))
+                    }
+                }
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (permissions.size == 1 &&
+                permissions[0] == Manifest.permission.ACCESS_COARSE_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                gMap.setMyLocationEnabled(true);
+
+            } else if (permissions.size > 1 &&
+                (permissions[0] == Manifest.permission.ACCESS_COARSE_LOCATION &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) ||
+                (permissions[1] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            ) {
+                gMap.setMyLocationEnabled(true);
+            } else {
+                val toast = Toast.makeText(
+                    this,
+                    "App needs location permision to function",
+                    Toast.LENGTH_LONG
+                )
+                toast.show()
+            }
+        }
+    }
+
+
+    fun isLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+    }
+
 }
